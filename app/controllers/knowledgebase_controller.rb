e16e6ddc460bfd4014985457893d86ad7c86d126
Rove Monteux <rove.monteux@fluid-rock.com>
2014-05-01 18:01:17 +0200

    show a text-only version of the frontpage of Le Monde (www.lemonde.fr) in our front page

--- app/controllers/knowledgebase_controller.rb
+++ app/controllers/knowledgebase_controller.rb
@@ -1,5 +1,7 @@
+require 'open-uri'
+
 class KnowledgebaseController < ApplicationController
   def index
-  
+	@paragraphs = Nokogiri::HTML.parse(open("http://www.lemonde.fr/")).css('p')
   end
 end
